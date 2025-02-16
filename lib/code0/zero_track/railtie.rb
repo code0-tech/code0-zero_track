6edef055aba195e5f611f045e2b38da9698135b7
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-02-16 22:21:14 +0100

    Add active record schema cleaner

--- lib/code0/zero_track/railtie.rb
+++ lib/code0/zero_track/railtie.rb
@@ -7,6 +7,12 @@ module Code0
       config.zero_track.active_record = ActiveSupport::OrderedOptions.new
       config.zero_track.active_record.timestamps = true
       config.zero_track.active_record.schema_migrations = true
+      config.zero_track.active_record.schema_cleaner = true
+
+      rake_tasks do
+        path = File.expand_path(__dir__)
+        Dir.glob("#{path}/../../tasks/**/*.rake").each { |f| load f }
+      end
 
       initializer 'code0.zero_track.inject' do
         Injectors::ActiveRecordTimestamps.inject! if config.zero_track.active_record.timestamps
