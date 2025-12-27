b677c29365c605400d1e143b7d4685e63e9d7ee9
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-12-27 16:01:29 +0100

    Add compatibility with rails 8.1

--- lib/code0/zero_track/injectors/active_record_schema_migrations.rb
+++ lib/code0/zero_track/injectors/active_record_schema_migrations.rb
@@ -4,12 +4,17 @@ module Code0
   module ZeroTrack
     module Injectors
       class ActiveRecordSchemaMigrations
-        def self.inject!
-          # Patch to write version information as empty files under the db/schema_migrations directory
-          # This is intended to reduce potential for merge conflicts in db/structure.sql
-          ActiveSupport.on_load(:active_record_postgresqladapter) do
-            prepend Database::PostgresqlAdapter::DumpSchemaVersionsMixin
+        def self.inject!(config)
+          if Rails.gem_version >= Gem::Version.create('8.1')
+            config.active_record.schema_versions_formatter = Database::SchemaMigrations::Formatter
+          else
+            # Patch to write version information as empty files under the db/schema_migrations directory
+            # This is intended to reduce potential for merge conflicts in db/structure.sql
+            ActiveSupport.on_load(:active_record_postgresqladapter) do
+              prepend Database::PostgresqlAdapter::DumpSchemaVersionsMixin
+            end
           end
+
           # Patch to load version information from empty files under the db/schema_migrations directory
           ActiveRecord::Tasks::PostgreSQLDatabaseTasks
             .prepend Database::PostgresqlDatabaseTasks::LoadSchemaVersionsMixin
